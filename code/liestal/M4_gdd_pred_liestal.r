81a03e435962b514a6e53c61a6f1ddd0ba7ab8cd
Sonny Min <42934656+joosung-min@users.noreply.github.com>
2023-02-23 05:02:12 +0000

    forecast

--- code/liestal/M4_gdd_pred_liestal.r
+++ code/liestal/M4_gdd_pred_liestal.r
@@ -1,10 +1,9 @@
 library(tidyverse)
 source("./code/_shared/F01_functions.r")
 # Make prediction for Liestal 2023 using the chill-day model from Jung et al. (2019)
-# From grid search we found the optimal parameters to be:
-# - Tc = 7
-# Rc_thresh = -90
-# Rh_thresh = 145
+# Tc = 7
+# Rc_thresh = -110
+# Rh_thresh = 126
 # Tc_fixed at Jan 31.
 
 # Load 2023 temperature data
@@ -19,23 +18,15 @@ liestal_2023$daily_Ca <- apply(liestal_2023, MARGIN = 1, FUN = F01_chill_days, T
 liestal_2023$AGDD <- cumsum(liestal_2023$daily_Ca)
 
 # Find the first day that AGDD > Rh_thresh
-Rc_thresh <- -90
-Rh_thresh <- 145
+Rc_thresh <- -110
+Rh_thresh <- 126
 
 bud_activation_idx <- which(liestal_2023$AGDD > -Rc_thresh)[1]
 bud_activation_day <- liestal_2023[bud_activation_idx, "date"]
 bud_activation_day
 
 full_bloom_idx <- which(liestal_2023$AGDD > Rh_thresh)[1]
-full_bloom_day <- liestal_2023[Rh_thresh_loc, "date"] 
-Rh_thresh_day # full-bloom day
+full_bloom_day <- liestal_2023[full_bloom_idx, "date"] 
+full_bloom_day  # full-bloom day
 
 strftime(full_bloom_day, format = "%j")
-
-cherry_sub <- read.csv("./code/_shared/data/A11_cherry_sub.csv") %>%
-    filter(city == "Liestal") %>%
-    filter(year > 1982)
-tail(cherry_sub, 10)
-ggplot(aes(x = year, y = bloom_doy), data = cherry_sub) + 
-    geom_line()
-sort(unique(cherry_sub$year))
