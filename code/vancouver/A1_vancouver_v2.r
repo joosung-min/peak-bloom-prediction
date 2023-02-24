4d15575291bcf789ca30764bd2d3de98b08061d8
Sonny Min <42934656+joosung-min@users.noreply.github.com>
2023-02-24 17:18:52 +0000

    changes

--- code/vancouver/A1_vancouver_v2.r
+++ code/vancouver/A1_vancouver_v2.r
@@ -106,15 +106,15 @@ write.csv(cherry_temp, "./code/_shared/data/A11_cherry_temp.csv", row.names = FA
 
 # Merge all the data together.
 # - Temperature, bloom_doy
-cherry_temp2 <- read.csv("./code/_shared/data/A11_cherry_temp2.csv") %>%
+cherry_temp2 <- read.csv("./code/_shared/data/A11_cherry_temp.csv") %>%
     merge(y = city_station_pair, by = "id", all.x = TRUE) %>%
     merge(y = cherry_sub %>% 
         select(city, bloom_doy, bloom_date)
         , by.x = c("city", "date")
         , by.y = c("city", "bloom_date"), all.x = TRUE)
 write.csv(cherry_temp2, "./code/_shared/data/A11_cherry_temp.csv", row.names = FALSE)
-# - lat, long, alt
 
+# - lat, long, alt
 city_locs <- cherry_sub[ ,c("city", "lat", "long", "alt")] %>% distinct(.keep_all = TRUE)
 
 cherry_temp3 <- read.csv("./code/_shared/data/A11_cherry_temp.csv") %>%
@@ -124,90 +124,3 @@ cherry_temp3 <- read.csv("./code/_shared/data/A11_cherry_temp.csv") %>%
     distinct(.keep_all = TRUE)
 
 write.csv(cherry_temp3, "./code/_shared/data/A11_cherry_complete.csv", row.names = FALSE)
-
-# Train-test split.
-# - Train: 1952-2012
-# - Test: 2013-2022
-# - Shuffle the data.
-# cherry_temp3 <- read.csv("./code/_shared/data/A11_cherry_complete.csv")
-# cherry_train <- cherry_temp2 %>% filter(year < 2013)
-# cherry_test <- cherry_temp2 %>% filter(year >= 2013)
-
-# set.seed(42)
-# cherry_train <- cherry_train[sample(nrow(cherry_train)), ]
-# cherry_test <- cherry_test[sample(nrow(cherry_test)), ]
-
-# # Train Elastic net
-# # - Response: is_bloom(classification), bloom_doy(regression)
-# # - Features: AGDD, daily_GDD, tmin, tmax, long, lat, alt
-# # - Perform 10-fold cross-validation.
-# # - Parameters for Elastic net: alpha, lambda
-# feature_names <- c("AGDD", "daily_GDD", "tmin", "tmax", "long", "lat", "alt")
-# target_col <- "is_bloom"
-
-# library(glmnet)
-# library(caret)
-
-# trainCtrl <- trainControl(
-#     method = "repeatedcv"
-#     , number = 10
-#     , repeats = 5
-#     , summaryFunction = twoClassSummary
-#     , classProbs = TRUE
-#     )
-
-# elastic_fit <- train(
-#     x = cherry_train[, feature_names]
-#     , y = cherry_train[, target_col]
-#     , method = "glmnet"
-#     , trControl = trainCtrl
-#     # , tuneLength = 30
-#     , tuneGrid =expand.grid(
-#         alpha=seq(0, 1, length = 10)
-#         , lambda = seq(exp(-6), exp(-2),length = 10))
-#     , family = "binomial"
-#     , preProcess = c("scale")
-# )
-# save(elastic_fit, file = "./code/_shared/data/A11_elastic_fit.RData")
-
-# # Regression model
-# cherry_train_reg <- cherry_train %>%filter(is_bloom == 1)
-# cherry_test_reg <- cherry_test %>%filter(is_bloom == 1)
-# target_col_reg <- "bloom_doy"
-
-# trainCtrl_reg <- trainControl(
-#     method = "repeatedcv"
-#     , number = 10
-#     , repeats = 5
-#     )
-
-# elastic_reg <- train(
-#     x = cherry_train[, feature_names]
-#     , y = cherry_train[, target_col_reg]
-#     , method = "glmnet"
-#     , trControl = trainCtrl_reg
-#     , tuneGrid =expand.grid(
-#         alpha=seq(0, 1, length = 10)
-#         , lambda = seq(exp(-6), exp(-2),length = 10))
-#     , family = "gaussian"
-#     , preProcess = c("scale")
-# )
-# save(elastic_fit, elastic_reg
-#     , file = "./code/_shared/data/A11_elastic_fitreg.RData")
-
-# library(doMC)
-# registerDoMC(cores = detectCores() - 1)
-# elastic_cv <- cv.glmnet(
-#     x = cherry_train[, feature_names]
-#     , y = cherry_train[, target_col]
-#     , family = "binomial"
-#     # , alpha = 1
-#     , nfolds = 10
-#     , type.measure = "auc" # "class"
-#     , parallel = TRUE
-# )
-# plot(elastic_cv)
-
-# Fit the best model.
-
-# Model evaluation.
